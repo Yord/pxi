8b34c75e0081f129867d349d591c083ecebd148b
Philipp Wille <philipp.y.wille@gmail.com>
2019-11-30 16:58:22 +0100

    refactor: be more precise and rename data to buffer

--- src/run.js
+++ src/run.js
@@ -1,11 +1,11 @@
 module.exports = (lex, parse, update) => {
   process.stdin.setEncoding('utf8')
 
-  let data = ''
+  let buffer = ''
 
   process.stdin
   .on('data', chunk => {
-    const {tokens, lines, rest} = lex(data + chunk)
+    const {tokens, lines, rest} = lex(buffer + chunk)
     const {parseErr, jsons}     = parse(tokens, lines)
     const {updateErr, out}      = update(jsons, lines)
 
@@ -13,7 +13,7 @@ module.exports = (lex, parse, update) => {
     if (updateErr !== '') process.stderr.write(updateErr + '\n')
     process.stdout.write(out)
 
-    data = rest
+    buffer = rest
   })
   .on('end', () => process.exit(0))
 }
