966fdb8a5e245413c7eb7f597fb347023ba99934
Philipp Wille <philipp.y.wille@gmail.com>
2019-11-30 15:48:18 +0100

    feat: enable defining custom parsers, lexers, and updaters in .fxrc

--- index.js
+++ index.js
@@ -2,41 +2,64 @@
 
 require('./src/fxrc')
 
-const _mapUpdater            = require('./src/updaters/map')
-const _flatMapUpdater        = require('./src/updaters/flatMap')
-const _bulkParser            = require('./src/parsers/bulk')
-const _singleParser          = require('./src/parsers/single')
-const _lineLexer             = require('./src/lexers/line')
-const _streamObjectLexer     = require('./src/lexers/stream/object')
+const _process                = require('process')
 
-const _argv                  = require('./src/args')
-const _run                   = require('./src/run')
+const _map                    = require('./src/updaters/map')
+const _flatMap                = require('./src/updaters/flatMap')
+const _bulk                   = require('./src/parsers/bulk')
+const _single                 = require('./src/parsers/single')
+const _line                   = require('./src/lexers/line')
+const _streamObject           = require('./src/lexers/streamObject')
 
-const _functionString        = _argv.f || 'json => json'
-const _spaces                = _argv.s || 0
-const _replacerString        = _argv.r || null
-const _singleParsing         = _argv.p || false
-const _failEarly             = _argv.e || false
-const _flatMapSemantics      = _argv.m || false
-const _verbose               = _argv.v || false
-const _lexer                 = _argv.l || 'line'
+const _argv                   = require('./src/args')
+const _run                    = require('./src/run')
 
-const _f                     = eval(_functionString)
-const _replacer              = eval(_replacerString)
+const _failEarly              = _argv.e || false
+const _functionString         = _argv.f || 'json => json'
+const _lexer                  = _argv.l || 'line'
+const _parser                 = _argv.p || 'bulk'
+const _replacerString         = _argv.r || null
+const _spaces                 = _argv.s || 0
+const _updater                = _argv.u || 'map'
+const _verbose                = _argv.v || false
 
-const _lex = (
-  _lexer === 'stream-object' ? _streamObjectLexer(_verbose)
-                             : _lineLexer(_verbose)
-)
+const _f                      = eval(_functionString)
+const _replacer               = eval(_replacerString)
 
-const _concatJsonStrs = (
-  _flatMapSemantics          ? _flatMapUpdater(_f, _replacer, _spaces)
-                             : _mapUpdater(_f, _replacer, _spaces)
-)
+let _lex = undefined
+try {
+  _lex = (
+    _lexer === 'streamObject' ? _streamObject(_verbose) :
+    _lexer === 'line'         ? _line(_verbose)
+                              : eval(_lexer)(_verbose)
+  )
+} catch(err) {
+  _process.stderr.write('No lexer defined with name ' + _lexer + '!\n')
+  _process.exit(1)
+}
 
-const _parse = (
-  _singleParsing             ? _singleParser(_verbose, _failEarly, _concatJsonStrs)
-                             : _bulkParser(_verbose, _failEarly, _concatJsonStrs)
-)
+let _update = undefined
+try {
+  _update = (
+    _updater === 'map'     ? _map(_f, _replacer, _spaces) :
+    _updater === 'flatMap' ? _flatMap(_f, _replacer, _spaces)
+                           : eval(_updater)(_f, _replacer, _spaces)
+  )
+} catch(err) {
+  _process.stderr.write('No updater defined with name ' + _updater + '!\n')
+  _process.exit(1)
+}
+
+let _parse = undefined
+try {
+  _parse = (
+    _parser === 'bulk'   ? _bulk(_verbose, _failEarly, _update) :
+    _parser === 'single' ? _single(_verbose, _failEarly, _update)
+                         : eval(_parser)(_verbose, _failEarly, _update)
+  )
+} catch(err) {
+  _process.stderr.write('No parser defined with name ' + _parser + '!\n')
+  _process.exit(1)
+}
 
 _run(_lex, _parse)
