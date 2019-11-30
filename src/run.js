445a43a9d52b743530ffb1037db65d91b80e42c7
Philipp Wille <philipp.y.wille@gmail.com>
2019-11-30 14:15:33 +0100

    refactor: move lexer, updater, and parser selection from run to index to improve separation of concerns

--- src/run.js
+++ src/run.js
@@ -1,30 +1,4 @@
-module.exports = function (functionString, spaces, replacerString, singleParsing, failEarly, flatMapSemantics, verbose, lexer) {
-  const mapUpdater        = require('./updaters/map')
-  const flatMapUpdater    = require('./updaters/flatMap')
-  const bulkParser        = require('./parsers/bulk')
-  const singleParser      = require('./parsers/single')
-  const lineLexer         = require('./lexers/line')
-  const streamObjectLexer = require('./lexers/stream/object')
-
-  const f = eval(functionString)
-  const replacer = eval(replacerString)
-  
-  const concatJsonStrs = (
-    flatMapSemantics ? flatMapUpdater(f, replacer, spaces)
-                     : mapUpdater(f, replacer, spaces)
-  )
-
-  const parse = (
-    singleParsing ? singleParser(verbose, failEarly, concatJsonStrs)
-                  : bulkParser(verbose, failEarly, concatJsonStrs)
-  )
-
-  const lex = (
-    lexer === 'stream-object' ? streamObjectLexer(verbose) :
-    lexer === 'line'          ? lineLexer(verbose)
-                              : lineLexer(verbose)
-  )
-
+module.exports = (lex, parse) => {
   process.stdin.setEncoding('utf8')
 
   let data = ''
@@ -35,7 +9,7 @@ module.exports = function (functionString, spaces, replacerString, singleParsing
 
     data = rest
 
-    const {out, err} = parse(tokens, lines)
+    const {err, out} = parse(tokens, lines)
 
     process.stderr.write(err)
     process.stdout.write(out)
