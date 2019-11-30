f9f822bc1297f7730357ee5e6028c974795e4e54
Philipp Wille <philipp.y.wille@gmail.com>
2019-11-30 14:16:32 +0100

    refactor: straighten out function declarations

--- src/lexers/line.js
+++ src/lexers/line.js
@@ -1,42 +1,40 @@
-module.exports = function (verbose) {
-  return data => {
-    const tokens = []
-    const lines  = []
+module.exports = verbose => data => {
+  const tokens = []
+  const lines  = []
 
-    let text     = data
-    let len      = text.length
+  let text     = data
+  let len      = text.length
 
-    let at       = -1
-    let line     = 1
-    
-    let obj      = false
+  let at       = -1
+  let line     = 1
+  
+  let obj      = false
 
-    let done     = false
-    let ch
-    
-    do {
-      at++
-      ch = text.charAt(at)
+  let done     = false
+  let ch
+  
+  do {
+    at++
+    ch = text.charAt(at)
 
-      if (ch === '\n') {
-        if (verbose) line++
-        obj = true
-      }
+    if (ch === '\n') {
+      if (verbose) line++
+      obj = true
+    }
 
-      if (at === len) done = true
+    if (at === len) done = true
 
-      if (obj) {
-        obj = false
-        const token = text.slice(0, at)
-        tokens.push(token)
-        if (verbose) lines.push(line)
+    if (obj) {
+      obj = false
+      const token = text.slice(0, at)
+      tokens.push(token)
+      if (verbose) lines.push(line)
 
-        text = text.slice(at + 1, len)
-        len = text.length
-        at = -1
-      }
-    } while (!done)
+      text = text.slice(at + 1, len)
+      len = text.length
+      at = -1
+    }
+  } while (!done)
 
-    return {tokens, lines, rest: text}
-  }
+  return {tokens, lines, rest: text}
 }
--- src/updaters/flatMap.js
+++ src/updaters/flatMap.js
@@ -1,14 +1,12 @@
-module.exports = function (f, replacer, spaces) {
-  return (jsonStrs, obj) => {
-    let str = jsonStrs
+module.exports = (f, replacer, spaces) => (jsonStrs, obj) => {
+  let str = jsonStrs
 
-    const obj2 = typeof f === 'undefined' ? obj : f(obj)
-    if (typeof obj2 !== 'undefined') {
-      if (Array.isArray(obj2)) {
-        for (const obj3 in obj2) str += JSON.stringify(obj3, replacer, spaces) + '\n'
-      } else str += JSON.stringify(obj2, replacer, spaces) + '\n'
-    }
-
-    return str
+  const obj2 = typeof f === 'undefined' ? obj : f(obj)
+  if (typeof obj2 !== 'undefined') {
+    if (Array.isArray(obj2)) {
+      for (const obj3 in obj2) str += JSON.stringify(obj3, replacer, spaces) + '\n'
+    } else str += JSON.stringify(obj2, replacer, spaces) + '\n'
   }
+
+  return str
 }
