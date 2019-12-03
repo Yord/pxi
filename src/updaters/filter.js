e5ded3337cfe68d4a16b41546587a3f4312f0481
Philipp Wille <philipp.y.wille@gmail.com>
2019-12-03 09:14:08 +0100

    feat: add filter updater

---
+++ src/updaters/filter.js
@@ -0,0 +1,20 @@
+module.exports = (verbose, failEarly, p, argv) => (jsons, lines) => {
+  let err = ''
+  let jsons2 = []
+
+  for (let index = 0; index < jsons.length; index++) {
+    try {
+      const obj = jsons[index]
+      if (typeof p === 'undefined' || p(obj) === true) jsons2.push(obj)
+    } catch (e) {
+      const line = verbose ? 'Line ' + lines[index] + ': ' : ''
+      err += line + e + '\n'
+      if (failEarly) {
+        process.stderr.write(err)
+        process.exit(1)
+      }
+    }
+  }
+
+  return {err, jsons: jsons2}
+}
