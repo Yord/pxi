72f6b56cdd1eb6fcec49d524389fcdb5688f3f9f
Philipp Wille <philipp.y.wille@gmail.com>
2019-12-03 09:14:30 +0100

    docs: update usage info

--- src/args.js
+++ src/args.js
@@ -9,76 +9,39 @@ module.exports = (
     'Shows this help message.'
   )
 
-  .alias('f', 'function')
-  .nargs('function', 1)
-  .describe(
-    'function',
-    'A string containing a JavaScript function "json => ..." that ' +
-    'is applied to each parsed JSON element. If you would like to ' +
-    'use libraries like lodash or ramda, read the documentation   ' +
-    'on ".fxrc" on the github page.'
-  )
-  
   .alias('l', 'lexer')
   .nargs('lexer', 1)
   .describe(
     'lexer',
-    'Defines which lexer is used to tokenize the input: "line"    ' +
-    '(default) or "streamObject". Line treats each line as one    ' +
-    'token. StreamObject parses streams of JSON objects (not      ' +
-    'arrays!) and drops any characters between them. If this      ' +
-    'setting gets any other value, it treats an identifier with   ' +
-    'this name as a lexer or fails, if none is found.'
+    'Defines how the input is tokenized: "line" (default) treats lines as tokens. "jsonStream" parses streams of JSON objects (not arrays!) and drops all characters in between. If --lexer gets any other string, the global scope is searched for a matching variable or function.'
   )
 
   .alias('p', 'parser')
   .nargs('parser', 1)
   .describe(
     'parser',
-    'Defines which parser is used to parse the tokens: "bulk"     ' +
-    '(default) or "single". Single parses each token on its own.  ' +
-    'Bulk parses all tokens in one go, which is faster, but fails ' +
-    'the whole bulk instead of just a single token if an error is ' +
-    'thrown. If this setting gets any other value, it treats an   ' +
-    'identifier with this name as a parser or fails, if none is   ' +
-    'found.'
+    'Defines how tokens are parsed into JSON: "single" parses each token individually. "bulk" (default) parses all tokens in one go, which is faster, but fails the whole bulk instead of just a single token if an error is thrown. If --parser gets any other string, the global scope is searched for a matching variable or function.'
+  )
+
+  .alias('f', 'function')
+  .nargs('function', 1)
+  .describe(
+    'function',
+    'Defines how JSON is transformed: "json => json" (default) If no function string is given, the identity function is used. "json => ..." All variables and functions in global scope may be used in the function. If you would like to use libraries like lodash or ramda, read the documentation on .pfrc on the github page.'
   )
 
   .alias('u', 'updater')
   .nargs('updater', 1)
   .describe(
     'updater',
-    'Defines which updater is used to apply f on the parsed JSON  ' +
-    'elements: "map" (default) or "flatMap". Map applies function ' +
-    'f to each parsed JSON element and replaces it with f\'s      ' +
-    'result. FlatMap also applies f to each element, but acts     ' +
-    'differently depending on f\'s result: On undefined, it drops ' +
-    'the JSON element. In case of an array, it returns every      ' +
-    'array item individually. For any other element, it acts like ' +
-    'map. If this setting gets any other value, it treats an      ' +
-    'identifier with this name as an updater or fails, if none is ' +
-    'found.'
+    'Defines how the the function f is applied to JSON: "map" (default) applies f to each parsed JSON element and replaces it with f\'s result. "flatMap" applies f to each element, but acts differently depending on f\'s result: On undefined return nothing. On [...] return every array item individually or nothing for empty arrays. Otherwise act like map. "filter" expects f to be a predicate and keeps all JSON elements for which f yields true. If --updater gets any other string, the global scope is searched for a matching variable or function.'
   )
 
   .alias('m', 'marshaller')
   .nargs('marshaller', 1)
   .describe(
     'marshaller',
-    'Defines which marshaller is used to transform the updated    ' +
-    'JSON elements to a string: Currently, only "stringify"       ' +
-    '(default) is implemented which uses JSON.stringify to print  ' +
-    'JSON elements. If this setting gets any other value, it      ' +
-    'treats an identifier with this name as a marshaller or       ' +
-    'fails, if none is found. Marshallers may define their own    ' +
-    'command line parameters and so does stringify:             \n' +
-    '-s, --spaces                                               \n' +
-    'The number of spaces used to format JSON. If it is set to 0  ' +
-    '(default), the JSON is printed in a single line.           \n' +
-    '-r, --replacer                                             \n' +
-    'The replacer is passed to stringify and determines which     ' +
-    'JSON fields are printed or left out. If it is set to null    ' +
-    '(default), all fields are printed. See the documentation of  ' +
-    'JSON.stringify to find out more.'
+    'Defines how the updated JSON is transformed back to a string: "stringify" (default) uses JSON.stringify and has the following additional options:\n\n-s, --spaces\nThe number of spaces used to format JSON. If it is set to 0 (default), the JSON is printed in a single line.\n\n-r, --replacer\nDetermines which JSON fields are kept. If it is set to null (default), all fields remain. See the documentation of JSON.stringify for details.\n\nIf --marshaller gets any other string, the global scope is searched for a matching variable or function.'
   )
 
   .alias('e', 'fail-early')
