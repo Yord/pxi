175e49231db64f87d52cd03170374c577e7607b2
Philipp Wille <philipp.y.wille@gmail.com>
2019-12-05 12:13:17 +0100

    refactor: adjust naming of errors to increase readability

--- src/run.js
+++ src/run.js
@@ -6,15 +6,15 @@ module.exports = (lex, parse, update, marshal) => {
 
   process.stdin
   .on('data', chunk => {
-    const {err: lexErr, tokens, lines, lastLine, rest} = lex(buffer + chunk, linesOffset)
-    const {err: parseErr, jsons}                       = parse(tokens, lines)
-    const {err: updateErr, jsons: jsons2}              = update(jsons, lines)
-    const {err: marshalErr, str}                       = marshal(jsons2)
+    const {err: lErr, tokens, lines, lastLine, rest} = lex(buffer + chunk, linesOffset)
+    const {err: pErr, jsons}                         = parse(tokens, lines)
+    const {err: uErr, jsons: jsons2}                 = update(jsons, lines)
+    const {err: mErr, str}                           = marshal(jsons2)
 
-    if (lexErr     !== '') process.stderr.write(lexErr     + '\n')
-    if (parseErr   !== '') process.stderr.write(parseErr   + '\n')
-    if (updateErr  !== '') process.stderr.write(updateErr  + '\n')
-    if (marshalErr !== '') process.stderr.write(marshalErr + '\n')
+    if (lErr !== '') process.stderr.write(lErr + '\n')
+    if (pErr !== '') process.stderr.write(pErr + '\n')
+    if (uErr !== '') process.stderr.write(uErr + '\n')
+    if (mErr !== '') process.stderr.write(mErr + '\n')
     process.stdout.write(str)
 
     buffer      = rest
