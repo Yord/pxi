635f5d734c9ba5b560636b7fa2f450923736412f
Philipp Wille <philipp.y.wille@gmail.com>
2019-12-05 12:12:33 +0100

    feat: add lines offset to lexers so lines in error messages are correct

--- src/run.js
+++ src/run.js
@@ -1,14 +1,15 @@
 module.exports = (lex, parse, update, marshal) => {
   process.stdin.setEncoding('utf8')
 
-  let buffer = ''
+  let buffer      = ''
+  let linesOffset = 0
 
   process.stdin
   .on('data', chunk => {
-    const {err: lexErr, tokens, lines, rest} = lex(buffer + chunk)
-    const {err: parseErr, jsons}             = parse(tokens, lines)
-    const {err: updateErr, jsons: jsons2}    = update(jsons, lines)
-    const {err: marshalErr, str}             = marshal(jsons2)
+    const {err: lexErr, tokens, lines, lastLine, rest} = lex(buffer + chunk, linesOffset)
+    const {err: parseErr, jsons}                       = parse(tokens, lines)
+    const {err: updateErr, jsons: jsons2}              = update(jsons, lines)
+    const {err: marshalErr, str}                       = marshal(jsons2)
 
     if (lexErr     !== '') process.stderr.write(lexErr     + '\n')
     if (parseErr   !== '') process.stderr.write(parseErr   + '\n')
@@ -16,7 +17,8 @@ module.exports = (lex, parse, update, marshal) => {
     if (marshalErr !== '') process.stderr.write(marshalErr + '\n')
     process.stdout.write(str)
 
-    buffer = rest
+    buffer      = rest
+    linesOffset = lastLine
   })
   .on('end', () => process.exit(0))
 }
