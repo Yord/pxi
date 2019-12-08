c16488211cd263088b59826514722204465846ad
Philipp Wille <philipp.y.wille@gmail.com>
2019-12-08 15:54:30 +0100

    fix: replace an occurrence of replacer where select was due

--- src/marshallers/json/stringify.js
+++ src/marshallers/json/stringify.js
@@ -2,9 +2,9 @@ module.exports = {
   name: 'jsonStringify',
   desc: 'uses JSON.stringify and has the following additional options:\n\n-S, --spaces\nThe number of spaces used to format JSON. If it is set to 0 (default), the JSON is printed in a single line.    [number]\n\n-s, --select\nDetermines which JSON fields are kept. If it is left out (default), all fields remain. If it is a string formatted as a JSON array, all fields in the array are kept. See the documentation of JSON.stringify for details.        [string]\n\n',
   func: (verbose, failEarly, argv) => {
-    const spaces    = argv.S || argv.spaces   || 0
-    const selectStr = argv.s || argv.replacer || null
-    const select    = JSON.parse(selectStr)   || null
+    const spaces    = argv.S || argv.spaces || 0
+    const selectStr = argv.s || argv.select || null
+    const select    = JSON.parse(selectStr) || null
 
     return jsons => {
       let err = ''
