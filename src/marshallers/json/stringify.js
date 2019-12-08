b2b9b5681e3d731a1de06e8e0fc1f33777945497
Philipp Wille <philipp.y.wille@gmail.com>
2019-12-08 14:25:07 +0100

    feat: rename replacer option in jsonStringify marshaller to select and update examples

--- src/marshallers/json/stringify.js
+++ src/marshallers/json/stringify.js
@@ -1,10 +1,10 @@
 module.exports = {
   name: 'jsonStringify',
-  desc: 'uses JSON.stringify and has the following additional options:\n\n-S, --spaces\nThe number of spaces used to format JSON. If it is set to 0 (default), the JSON is printed in a single line.    [number]\n\n-R, --replacer\nDetermines which JSON fields are kept. If it is set to null (default), all fields remain. See the documentation of JSON.stringify for details.                         [string]\n\n',
+  desc: 'uses JSON.stringify and has the following additional options:\n\n-S, --spaces\nThe number of spaces used to format JSON. If it is set to 0 (default), the JSON is printed in a single line.    [number]\n\n-s, --select\nDetermines which JSON fields are kept. If it is left out (default), all fields remain. If it is a string formatted as a JSON array, all fields in the array are kept. See the documentation of JSON.stringify for details.        [string]\n\n',
   func: (verbose, failEarly, argv) => {
-    const spaces      = argv.S || argv.spaces   || 0
-    const replacerStr = argv.R || argv.replacer || null
-    const replacer    = eval(replacerStr)       || null
+    const spaces    = argv.S || argv.spaces   || 0
+    const selectStr = argv.s || argv.replacer || null
+    const select    = JSON.parse(selectStr)   || null
 
     return jsons => {
       let err = ''
@@ -13,7 +13,7 @@ module.exports = {
       for (let index = 0; index < jsons.length; index++) {
         try {
           const obj = jsons[index]
-          str += JSON.stringify(obj, replacer, spaces) + '\n'
+          str += JSON.stringify(obj, select, spaces) + '\n'
         } catch (e) {
           const info = verbose > 1 ? ' while marshalling:\n' + JSON.stringify(obj, null, 2) : ''
           err += e + info + '\n'
