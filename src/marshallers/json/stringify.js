f2cbe27ef1a9869c20122a8c5a16a97f5c3f7feb
Philipp Wille <philipp.y.wille@gmail.com>
2019-12-08 08:43:52 +0100

    feat: use capital letters for arguments in plugins

--- src/marshallers/json/stringify.js
+++ src/marshallers/json/stringify.js
@@ -2,8 +2,8 @@ module.exports = {
   name: 'jsonStringify',
   desc: 'uses JSON.stringify and has the following additional options:\n\n-s, --spaces\nThe number of spaces used to format JSON. If it is set to 0 (default), the JSON is printed in a single line.\n\n-r, --replacer\nDetermines which JSON fields are kept. If it is set to null (default), all fields remain. See the documentation of JSON.stringify for details.\n\n',
   func: (verbose, failEarly, argv) => {
-    const spaces      = argv.s || argv.spaces   || 0
-    const replacerStr = argv.r || argv.replacer || null
+    const spaces      = argv.S || argv.spaces   || 0
+    const replacerStr = argv.R || argv.replacer || null
     const replacer    = eval(replacerStr)       || null
 
     return jsons => {
