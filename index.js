9dfb1aae3866a7d5c17f1ce0af889df2d4fa3dcf
Philipp Wille <philipp.y.wille@gmail.com>
2019-12-13 12:12:54 +0100

    chore: rename A to _api

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 #!/usr/bin/env node
 
 const _run       = require('./src/run')
-const A          = require('./src/api')
+const _api       = require('./src/api')
 
 const _base      = require('@pf/base')
 const _json      = require('@pf/json')
 const _PF        = require('./src/pfrc') || {}
 
 const _reference = {lexer: 'line', parser: 'jsonBulk', applicator: 'map', marshaller: 'jsonStringify'}
-const _defaults  = A.combineDefaults([_PF.defaults, _reference])
-const _plugins   = A.combinePlugins([_core, _json].concat(_PF.plugins || []))
+const _defaults  = _api.combineDefaults([_PF.defaults, _reference])
+const _plugins   = _api.combinePlugins([_base, _json].concat(_PF.plugins || []))
 
 const _argv      = require('./src/args')(_defaults, _plugins)
-const _fs        = A.initFunctions(_argv, _plugins, _defaults)
+const _fs        = _api.initFunctions(_argv, _plugins, _defaults)
 
 _run(_fs)
