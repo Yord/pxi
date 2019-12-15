d8dd184c282813173cb847da94f5666fe58641ed
Philipp Wille <philipp.y.wille@gmail.com>
2019-12-15 20:17:04 +0100

    feat: factor out runner and early initialize fail-early

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const _run         = require('./src/run')
+const _runner      = require('./src/runner')
 const _api         = require('./src/api')
 
 const _reference   = require('./reference.json')
@@ -12,9 +12,11 @@ const _defaults    = _api.combineDefaults([_PF.defaults, _reference.defaults])
 const _globalArgv  = require('./src/globalArgs')(process.argv.slice(2), _defaults)
 const _noPlugins   = typeof _globalArgv.noPlugins !== 'undefined' ? _globalArgv.noPlugins : false
 
-const _plugins     = _api.combinePlugins(_PF.plugins.concat(_noPlugins ? [] : _reference.plugins))
+const _plugins     = _api.combinePlugins((_PF.plugins || []).concat(_noPlugins ? [] : _reference.plugins))
 
 const _argv        = require('./src/args')(process.argv.slice(2), _defaults, _plugins)
 const _fs          = _api.initFunctions(_argv, _plugins, _defaults)
+
+const _run         = _runner(_argv.failEarly)
 
 _run(_fs)
