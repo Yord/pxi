8f526b241d5879c686185197d80a634e802be055
Philipp Wille <philipp.y.wille@gmail.com>
2019-12-28 16:20:05 +0100

    chore: refactor usage documentation and add --from alias to --parser and --to alias to --marshaller

--- src/args.js
+++ src/args.js
@@ -6,92 +6,81 @@ module.exports = (argv, {lexer, parser, applicator, marshaller}, {lexers, parser
   .usage(
     '$0 FUNCTIONS ... [OPTIONS ...] \n' +
     '\n' +
-    'FUNCTIONS define how JSON is transformed. If several functions are given, they  ' +
+    'FUNCTIONS define how JSON is transformed. If several functions are given, they '  +
     'are applied in order. If no function is given, "json => json" is used, instead. ' +
     'All variables and functions in global scope may be used in the function. If you ' +
     'would like to use libraries like lodash or ramda, read the .pfrc module section ' +
     'on the github page: https://github.com/Yord/pf#pfrc-module              [string]'
   )
 
-  .string('lexer')
-  .nargs('lexer', 1)
-  .alias('l', 'lexer')
-  .choices('lexer', lexers.map(plugin => plugin.name))
-  .describe(
-    'lexer',
-    'Defines how the input is split into tokens: ' +
-    describePlugins(lexers, lexer)
+  .group(
+    ['lexer', 'parser', 'applicator', 'marshaller', 'fail-early', 'no-plugins', 'verbose'],  
+    'OPTIONS:\n'
   )
 
-  .string('parser')
-  .nargs('parser', 1)
-  .alias('p', 'parser')
-  .choices('parser', parsers.map(plugin => plugin.name))
-  .describe(
-    'parser',
-    'Defines how tokens are parsed into JSON: ' +
-    describePlugins(parsers, parser)
+  .group(
+    ['help', 'version'],
+    'OTHERS:\n'
   )
 
-  .string('applicator')
-  .nargs('applicator', 1)
-  .alias('a', 'applicator')
-  .choices('applicator', applicators.map(plugin => plugin.name))
-  .describe(
-    'applicator',
-    'Defines how FUNCTIONS are applied to JSON: ' +
-    describePlugins(applicators, applicator)
-  )
+  .option('lexer', {
+    type:     'string',
+    nargs:    1,
+    alias:    ['l'],
+    choices:  lexers.map(plugin => plugin.name),
+    describe: '\nDefines how the input is split into tokens: ' + describePlugins(lexers, lexer) + '\n'
+  })
 
-  .string('marshaller')
-  .nargs('marshaller', 1)
-  .alias('m', 'marshaller')
-  .choices('marshaller', marshallers.map(plugin => plugin.name))
-  .describe(
-    'marshaller',
-    'Defines how the transformed JSON is converted to a string: ' +
-    describePlugins(marshallers, marshaller)
-  )
+  .option('parser', {
+    type:     'string',
+    nargs:    1,
+    alias:    ['from', 'p'],
+    choices:  parsers.map(plugin => plugin.name),
+    describe: '\nDefines how tokens are parsed into JSON: ' + describePlugins(parsers, parser) + '\n'
+  })
 
-  .boolean('fail-early')
-  .alias('e', 'fail-early')
-  .describe(
-    'fail-early',
-    'Usually, every error is caught and written to stderr. But if ' +
-    'this flag is set, only the first error is printed and the    ' +
-    'process exits with code 1.'
-  )
+  .option('applicator', {
+    type:     'string',
+    nargs:    1,
+    alias:    ['a'],
+    choices:  applicators.map(plugin => plugin.name),
+    describe: '\nDefines how FUNCTIONS are applied to JSON: ' + describePlugins(applicators, applicator) + '\n'
+  })
 
-  .boolean('no-plugins')
-  .describe(
-    'no-plugins',
-    'Disables all plugins except those added in the .pfrc module. ' +
-    'Useful for plugin development. BEWARE: You may need to set   ' +
-    'new defaults in the .pfrc module!'
-  )
+  .option('marshaller', {
+    type:     'string',
+    nargs:    1,
+    alias:    ['to', 'm'],
+    choices:  marshallers.map(plugin => plugin.name),
+    describe: '\nDefines how the transformed JSON is converted to a string: ' + describePlugins(marshallers, marshaller) + '\n'
+  })
+  
+  .option('fail-early', {
+    type:     'boolean',
+    describe: '\nUsually, every error is caught and written to stderr. But if this flag is set, only the first error is printed and the process exits with code 1.\n'
+  })
 
-  .count('verbose')
-  .alias('v', 'verbose')
-  .describe(
-    'verbose',
-    'Apply -v several times (-vv) to be more verbose. Level 1     ' +
-    'pints lines in parser and applicator error messages. Level 2 ' +
-    'also prints the tokens or JSON objects that failed to be     ' +
-    'parsed or transformed.'
-  )
+  .option('no-plugins', {
+    type:     'boolean',
+    describe: '\nDisables all plugins except those added in the .pfrc module. Useful for plugin development. BEWARE: You may need to set new defaults in the .pfrc module!\n'
+  })
 
-  .help('h')
-  .alias('h', 'help')
-  .describe(
-    'help',
-    'Shows this help message.'
-  )
+  .option('verbose', {
+    type:     'count',
+    alias:    ['v'],
+    describe: '\nApply -v several times (-vv) to be more verbose. Level 1 prints lines in parser and applicator error messages. Level 2 also prints the tokens or JSON objects that failed to be parsed or transformed.\n'
+  })
+
+  .help('help')
+  .option('help', {
+    alias:    ['h'],
+    describe: 'Shows this help message.\n'
+  })
 
   .version('version')
-  .describe(
-    'version',
-    'Show version number.'
-  )
+  .option('version', {
+    describe: 'Shows version number.\n'
+  })
 
   .epilog('Copyright (c) Philipp Wille 2019')
   .argv
@@ -105,7 +94,7 @@ function describePlugin (defaultName) {
   return plugin => {
     if (typeof plugin.name === 'undefined') return []
     else return [
-      '"' + plugin.name + '"' +
+      '\n"' + plugin.name + '"' +
       (plugin.name === defaultName ? ' (default) ' : ' ') +
       plugin.desc
     ]
